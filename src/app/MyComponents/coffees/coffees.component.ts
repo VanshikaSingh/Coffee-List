9c5ae059530df89907100980b4435275370d2a76
Vanshika Singh <vn975950@dal.ca>
2023-01-26 12:18:31 -0400

    OnPush changedetection added

--- src/app/MyComponents/coffees/coffees.component.ts
+++ src/app/MyComponents/coffees/coffees.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ApiserviceService } from '../services/apiservice.service';
 import {Store} from '@ngrx/store'
 import * as CoffeeActions from '../store/actions'
@@ -9,7 +9,8 @@ import { CoffeeStateInterface } from '../types/coffeeState.interface';
 @Component({
   selector: 'app-coffees',
   templateUrl: './coffees.component.html',
-  styleUrls: ['./coffees.component.css']
+  styleUrls: ['./coffees.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CoffeesComponent implements OnInit{
   title = 'Coffee';
@@ -18,9 +19,8 @@ export class CoffeesComponent implements OnInit{
   currentPage= 1;
   pageSize = 10;
 
-  constructor(private _apiservice: ApiserviceService,private store: Store ){
+  constructor(private _apiservice: ApiserviceService,private store: Store, private ref: ChangeDetectorRef ){
   }
-
   ngOnInit(){
     this._apiservice.getdata().subscribe( res =>{ 
       this.newData = res; 
@@ -28,9 +28,8 @@ export class CoffeesComponent implements OnInit{
          this.store.dispatch(CoffeeActions.setCoffeeList({coffeeList:this.newData}))
     })
      this.store.subscribe((list:any)=>{
-
       this.coffeeList=(<CoffeeStateInterface>list['coffees']).coffeeList;
-
+      this.ref.detectChanges();
      });
   }
 }
